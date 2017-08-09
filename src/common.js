39480f052a60c40b875f16a3a4abfe219318a23e
purampurushotham <puram.purushotham@india.semanticbits.com>
2017-08-09 10:42:50 +0530

    default data

--- src/common.js
+++ src/common.js
@@ -3,11 +3,32 @@
  */
 let common = {
   setValueToStore: function (data, path, value) {
+    if (Array.isArray(value)) {
+      console.log('value')
+      console.log(value)
+      console.log('value')
+      data[path] = data[path].concat(value)
+    } else {
+      console.log('object value')
+      console.log(value)
+      console.log('object value')
+      data[path].push(value)
+    }
+    console.log(data[path])
     console.log('set value to store')
-    console.log(data)
-    console.log(path)
-    console.log(value)
-    console.log('set value to store')
+  },
+  setSearchOptions: function (data, bloodgroups, cities) {
+    console.log('set search optiomns')
+    data.map((eachDonor, index) => {
+      if (!bloodgroups.includes(eachDonor.bloodGroup)) {
+        bloodgroups.push(eachDonor.bloodGroup)
+      }
+      if (!cities.includes(eachDonor.city)) {
+        cities.push(eachDonor.city)
+      }
+    })
+    console.log(window.bb.blood_groups)
+    console.log('set search optiomns')
   }
 }
 export default common
--- src/component.js
+++ src/component.js
@@ -3,10 +3,19 @@ import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '../styles/main.css'
+import DatePicker from 'vuejs-datepicker'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
 import actions from '../src/actions'
+import common from '../src/common'
+import grid from 'vue2-bootstrap-table'
+import Multiselect from 'vue-multiselect'
 import './data'
 Vue.use(VueRouter)
-window.bb = {}
+window.bb = {
+  donors: [],
+  blood_groups: [],
+  cities: []
+}
 // import { EventBus } from './components/eventBus'
 window.bb.routes = [
   {
@@ -20,31 +29,7 @@ window.bb.routes = [
       },
       data () {
         return {
-          bb: window.bb,
-          donor: {
-            id: '',
-            firstName: '',
-            lastName: '',
-            occupation: '',
-            bloodGroup: '',
-            city: '',
-            dob: '',
-            martial_status: '',
-            p_phone: '',
-            p_email: '',
-            e_email: '',
-            e_phone: '',
-            recentDonor: {
-              recentDonorTime: '',
-              reachedTime: '',
-              exists: false
-            }
-          }
-        }
-      },
-      methods: {
-        donorRegistration () {
-          console.log('this')
+          bb: window.bb
         }
       }
     }
@@ -57,6 +42,15 @@ window.bb.routes = [
         return { bb: window.bb }
       }
     }
+  },
+  {
+    path: '/search',
+    component: {
+      template: '#search-template',
+      data () {
+        return { bb: window.bb }
+      }
+    }
   }
 ]
 Vue.component('donor-app', {
@@ -64,9 +58,6 @@ Vue.component('donor-app', {
   created () {
     console.log(window.bb)
     console.log('header')
-    /*   EventBus.on('logged', (logs) => {
-     this.logged = logs
-     }) */
   },
   data () {
     return {
@@ -81,14 +72,100 @@ Vue.component('donor-app', {
     }
   }
 })
-Vue.component('sample', {
-  template: '#sample-template',
+Vue.component('donor-registration', {
+  template: '#donor-registration-template',
+  components: {
+    DatePicker
+  },
   data () {
     return {
-      bb: window.bb
+      bb: window.bb,
+      donor: {
+        id: '',
+        firstName: '',
+        lastName: '',
+        occupation: '',
+        bloodGroup: '',
+        city: '',
+        dob: '',
+        martial_status: '',
+        p_phone: '',
+        p_email: '',
+        e_email: '',
+        e_phone: '',
+        recentDonor: {
+          recentDonorTime: '',
+          reachedTime: '',
+          exists: false
+        }
+      }
+    }
+  },
+  mounted () {
+    console.log('created registration')
+    console.log('created registration')
+  },
+  methods: {
+    donorRegistration () {
+      common.setValueToStore(window.bb, 'donors', this.donor)
     }
   }
 })
+Vue.component('search-donor', {
+  template: '#search-donor-template',
+  components: {
+    grid,
+    Multiselect
+  },
+  created () {
+    console.log('created search')
+    console.log(window.bb.donors)
+    common.setSearchOptions(window.bb.donors, window.bb.blood_groups, window.bb.cities)
+    console.log('created search')
+  },
+  data () {
+    return {
+      bb: window.bb,
+      bloodGroup: '',
+      city: '',
+      blood_groups: window.bb.blood_groups,
+      cities: window.bb.cities,
+      columns: ['firstName', 'lastName', 'bloodGroup', 'city', 'occupation', 'dob', 'martial_status', 'edit'],
+      headers: [
+        {
+          title: 'First Name',
+          path: 'firstName'
+        }, {
+          title: 'Blood Group',
+          path: 'bloodGroup'
+        }, {
+          title: 'Last Name',
+          path: 'lastName'
+        }, {
+          title: 'Date Of Birth',
+          path: 'dob'
+        }, {
+          title: 'City',
+          path: 'city'
+        }
+      ],
+      tableData: window.bb.donors
+    }
+  },
+  methods: {
+    searchDonor () {
+      console.log('search donor')
+    },
+    rowClick (row) {
+      console.log(row)
+    },
+    reset () {
+      this.bloodGroup = ''
+      this.city = ''
+    }
+  }
+})
+
 let router = new VueRouter({
   mode: 'history',
   routes: window.bb.routes
@@ -97,7 +174,7 @@ window.blood_donor = new Vue({
   router: router,
   mounted () {
     console.log('mounted')
-    console.log(window)
+    console.log(window.remoteController)
     actions.populateDonors(function () {
       console.log('populate Donors')
     })
